22624793b452d62cd8d6b98a4086567e09cec01f
TriciaChimalu <ozobialupatricia@gmail.com>
2025-01-02 15:06:44 -0800

    create new files for testing

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
 const pages = document.querySelectorAll('.page');
 const prevButton = document.getElementById('prevPage');
 const nextButton = document.getElementById('nextPage');
+let saveButtton =document.getElementById('textarea-btn');
 let currentPage = 0;
 
-showPage()
-/*
-function showPage(index){
-    pages.forEach((page,i) => {
-        if(i === index){
-            
-            page.style.transform ='rotateY(0)';
-            page.style.zindex = pages.length - i
-        }else if(i < index){
-            page.style.transform = 'rotate(-180deg)';
-            page.style.zindex =i;
-        }else{
-            page.style.transform ='rotateY(0)';
-            page.style.zindex = pages.length - i;
-        }
-    })
-};*/
+
+
+saveButtton.addEventListener('click',inputMessage);
+
+function  inputMessage(){
+    
+    let inputMessage = document.getElementById('textArea').value;
+    let messageDiv  = document.createElement('div');
+    messageDiv.textContent = inputMessage;
+    pages.appendChild(messageDiv)
+}
 
 function showPage(){
     pages.forEach((page,index) => {
+        
         if(index <= currentPage){
+    
             page.classList.add('flipped');
-            page.style.zIndex =index;
+            page.style.zIndex = index ;
 
         }
     else{
         page.classList.remove('flipped');
         page.style.zIndex = pages.length - index;
     }
+    //console.log(`page ${index + 1} flipped`);
+   
 });
 }
 
 nextButton.addEventListener('click',() =>{
-    
     if(currentPage < pages.length - 1){
         currentPage++;
         showPage()
